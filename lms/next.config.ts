badfe82c90628bfc9502399dd1357a78c7645f7d
Akhil Raj <akhilrajuvysyaraju19.com>
2026-02-07 20:09:58 +0530

    deploy1

--- lms/next.config.ts
+++ lms/next.config.ts
@@ -15,6 +15,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
     ],
   },
 };
